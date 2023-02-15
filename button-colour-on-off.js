bd7158a8d4237975b7d8b6df5e16226b3d6b9199
jacksonkoh8 <74502141+jacksonkoh8@users.noreply.github.com>
2023-02-16 00:12:38 +0800

    4rd

--- button-colour-on-off.js
+++
@@ -1,7 +0,0 @@
-var header = document.getElementById("bottomButtons");
-var buttons = header.getElementsByClassName("button");
-for (let i = 0, l = buttons.length; i < l; i++) {
-  buttons[i].addEventListener('click', function() {
-    buttons[i].classList.toggle('active');
-  })
-}
